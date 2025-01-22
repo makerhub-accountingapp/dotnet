68a43d03caf20ad9cbb14d31ad5d20396e5f1511
Yuki MARCHAL <yuki.marchal1216@gmail.com>
2025-01-22 14:31:02 +0100

    hub added

---
+++ AccountingApp.API/Hubs/DetailHub.cs
@@ -0,0 +1,22 @@
+﻿using AccountingApp.BLL.Interfaces;
+using AccountingApp.BLL.Services;
+using AccountingApp.DB.Entities;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AccountingApp.API.Hubs
+{
+	public class DetailHub(IDetailService service) : Hub
+	{
+		private IEnumerable<Detail> Details => service.Get();
+
+		public async Task Refresh()
+		{
+			await Clients.All.SendAsync("GetDetails", Details);
+		}
+
+		public async Task Get()
+		{
+			await Clients.Caller.SendAsync("GetDetails", Details);
+		}
+	}
+}
--- AccountingApp.API/Program.cs
+++ AccountingApp.API/Program.cs
@@ -1,3 +1,8 @@
+using AccountingApp.API.Hubs;
+using AccountingApp.BLL.Interfaces;
+using AccountingApp.BLL.Services;
+using AccountingApp.DAL.Interfaces;
+using AccountingApp.DAL.Repositories;
 using AccountingApp.DB.Contexts;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,19 +13,33 @@ var connectionString = builder.Configuration.GetConnectionString("Default");
 // Add services to the container.
 builder.Services.AddDbContext<MainContext>(options => {
 	options.UseNpgsql(connectionString);
-	// To avoid a loop ParentObject { ChildObject { ParentObject {...} } }
+	// Avoids a loop ParentObject { ChildObject { ParentObject {...} } }
 	options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
 
 builder.Services.AddControllers()
-	// To avoid a loop ParentObject { ChildObject { ParentObject {...} } }
+	// Avoids a loop ParentObject { ChildObject { ParentObject {...} } }
 	.AddJsonOptions(options
 		=> options.JsonSerializerOptions.ReferenceHandler
 		= System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
 
+builder.Services.AddScoped<IDetailRepository, DetailRepository>();
+builder.Services.AddScoped<IDetailService, DetailService>();
+
+builder.Services.AddSignalR();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configures external access
+builder.Services.AddCors(c => c.AddDefaultPolicy(options =>
+{
+	options.AllowCredentials()
+	.AllowAnyHeader()
+	.AllowAnyMethod()
+	.WithOrigins("http://localhost:4200", "http://192.168.0.201:4200");
+}));
 
 var app = builder.Build();
 
@@ -33,8 +52,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Hubs
+app.MapHub<DetailHub>("/hub/Detail");
 
 app.Run();

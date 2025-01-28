774e864e598c42b60d82c948e8ffd84c5d3f0de2
Yuki MARCHAL <yuki.marchal1216@gmail.com>
2025-01-28 15:38:46 +0100

    created hub template

--- AccountingApp.API/Hubs/DetailHub.cs
+++ AccountingApp.API/Hubs/DetailHub.cs
@@ -1,30 +1,12 @@
-﻿using AccountingApp.BLL.Interfaces;
+﻿using AccountingApp.API.Templates;
+using AccountingApp.BLL.Interfaces;
 using AccountingApp.BLL.Services;
 using AccountingApp.DB.Entities;
 using Microsoft.AspNetCore.SignalR;
 
 namespace AccountingApp.API.Hubs
 {
-	public class DetailHub(IDetailService service) : Hub
-	{		
-		public async Task NotifyGet()
-		{
-			await Clients.Caller.SendAsync("ReceiveGet", "Details retrieved");
-		}
-
-		public async Task NotifyCreate()
-		{
-			await Clients.All.SendAsync("ReceiveCreate", "Detail created");
-		}
-
-		public async Task NotifyUpdate()
-		{
-			await Clients.All.SendAsync("ReceiveUpdate", "Detail updated");
-		}
-
-		public async Task NotifyDelete()
-		{
-			await Clients.All.SendAsync("ReceiveDelete", "Detail deleted");
-		}
+	public class DetailHub : BaseHub<Detail>
+	{
 	}
 }
---
+++ AccountingApp.API/Templates/BaseHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AccountingApp.API.Templates
+{
+	public class BaseHub<TEntity> : Hub, IBaseHub<TEntity>
+		where TEntity : class
+	{
+		public async Task NotifyCreate(TEntity entity)
+		{
+			await Clients.Caller.SendAsync($"ReceiveCreate{typeof(TEntity).Name}", entity);
+		}
+
+		public async Task NotifyDelete(TEntity entity)
+		{
+			await Clients.All.SendAsync($"ReceiveDelete{typeof(TEntity).Name}", entity);
+		}
+
+		public async Task NotifyGet(TEntity[] entities)
+		{
+			await Clients.All.SendAsync($"ReceiveGet{typeof(TEntity).Name}", entities);
+		}
+
+		public async Task NotifyGetById(TEntity entity)
+		{
+			await Clients.All.SendAsync($"ReceiveGet{typeof(TEntity).Name}ById", entity);
+		}
+
+		/********** Note **********/
+
+		// Better to separate hub and controller even if it is possible only to use controller
+		// Controllelr => request-response model
+		// Hub => real-time communication
+		// Logics concerning real-time communication should be written in hub
+		// Keep the code clean and sustainable
+	}
+}

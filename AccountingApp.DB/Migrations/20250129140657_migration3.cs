70be67ea8c7ffeda5681660d05d44c721d6213f7
Yuki MARCHAL <yuki.marchal1216@gmail.com>
2025-02-10 10:15:08 +0100

    develop merged into features/detail

---
+++ AccountingApp.DB/Migrations/20250129140657_migration3.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountingApp.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TransactionDetailId",
+                table: "Category",
+                newName: "DetailId");
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Repetition",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Repetition",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Repetition",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Repetition",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Repetition",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Repetition",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Repetition",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Repetition",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Repetition",
+                value: 4);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DetailId",
+                table: "Category",
+                newName: "TransactionDetailId");
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Repetition",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Repetition",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Repetition",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Repetition",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Repetition",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Repetition",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Repetition",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Repetition",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Repetition",
+                value: 3);
+        }
+    }
+}
